af8a4b46d969e0a16496d749aaa8272dbc32cc33
Simeon <simeon.went@gmail.com>
2018-10-17 14:56:43 +0200

    Added json responce

---
+++ app/src/main/java/com/efarmer/erain/Todays_Weather.java
@@ -0,0 +1,13 @@
+package com.efarmer.erain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Todays_Weather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todays__weather);
+    }
+}
