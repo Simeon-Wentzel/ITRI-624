af5593cd7d44f9683568eb27e32370e22cd580f5
Dehan <dehanduvenhage@gmail.com>
2018-10-19 16:07:15 +0200

    Geo Location and unix time

---
+++ app/src/main/java/com/efarmer/erain/LatLng_test.java
@@ -0,0 +1,83 @@
+package com.efarmer.erain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+class LatLng_test implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public static final int DEFAULT_PRECISION = 6;
+
+    private BigDecimal lat, lng;
+
+    public LatLng_test(final BigDecimal lat, final BigDecimal lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public LatLng_test(final String lat, final String lng) {
+        this.lat = new BigDecimal(lat);
+        this.lng = new BigDecimal(lng);
+    }
+
+    public BigDecimal getLat() {
+        return lat;
+    }
+
+    public void setLat(BigDecimal lat) {
+        this.lat = lat;
+    }
+
+    public BigDecimal getLng() {
+        return lng;
+    }
+
+    public void setLng(BigDecimal lng) {
+        this.lng = lng;
+    }
+
+    /**
+     * @return Returns a string of the form "lat,lng" for this LatLng_test. We round the lat/lng values to 6 decimal places by default.
+     */
+    public String toUrlValue() {
+        return toUrlValue(DEFAULT_PRECISION);
+    }
+
+    /**
+     * @param precision We round the lat/lng values
+     * @return Returns a string of the form "lat,lng" for this LatLng_test.
+     */
+    public String toUrlValue(int precision) {
+        return lat.setScale(precision, BigDecimal.ROUND_HALF_EVEN).toString() + "," + lng.setScale(precision, BigDecimal.ROUND_HALF_EVEN).toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LatLng_test latLng = (LatLng_test) o;
+
+        if (lat != null ? !lat.equals(latLng.lat) : latLng.lat != null) return false;
+        if (lng != null ? !lng.equals(latLng.lng) : latLng.lng != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lat != null ? lat.hashCode() : 0;
+        result = 31 * result + (lng != null ? lng.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("LatLng_test");
+        sb.append("{lat=").append(lat);
+        sb.append(", lng=").append(lng);
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- app/src/main/java/com/efarmer/erain/Todays_Weather.java
+++ app/src/main/java/com/efarmer/erain/Todays_Weather.java
@@ -1,5 +1,7 @@
 package com.efarmer.erain;
 
+import android.location.Address;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,15 +17,25 @@ import com.google.gson.GsonBuilder;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.location.Geocoder;
+
 
 import darkSkyGson.Alerts;
 import darkSkyGson.Currently;
 
+
 public class Todays_Weather extends AppCompatActivity {
 
     private Gson gson;
-    private String lat, lon;
-    private String URL = "https://api.darksky.net/forecast/5ef650b8d20db9df7d7a8c83e3a13af8/"+lat+","+lon+"?exclude=minutely,hourly,daily,alerts,flags";
+    Long tsLong = System.currentTimeMillis()/1000;
+    String ts = tsLong.toString();
+
+    private String URL;
     TextView precipitationOutput, humid;
 
     Currently currently = new Currently();
@@ -34,6 +46,29 @@ public class Todays_Weather extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todays__weather);
+        Log.e("Time", String.format("%s ", ts));
+        if(Geocoder.isPresent()){
+            try {
+                String location = "Potchefstroom";
+                Geocoder gc = new Geocoder(this);
+                List<Address> addresses= gc.getFromLocationName(location, 5); // get the found Address Objects
+
+                List<LatLng> ll = new ArrayList<LatLng>(addresses.size()); // A list to save the coordinates if they are available
+                for(Address a : addresses){
+                    if(a.hasLatitude() && a.hasLongitude()){
+                        ll.add(new LatLng(a.getLatitude(), a.getLongitude()));
+
+                        Log.e("Lat", String.format("%s ", a.getLatitude()));
+
+                        Log.e("Lon", String.format("%s ", a.getLongitude()));
+                        URL = "https://api.darksky.net/forecast/5ef650b8d20db9df7d7a8c83e3a13af8/"+a.getLatitude()+","+a.getLongitude()+","+ ts +"?exclude=current,flags";
+                    }
+                }
+
+            } catch (IOException e) {
+                // handle the exception
+            }
+        }
 
         precipitationOutput = (TextView) findViewById(R.id.lblPrecipitationOutput);
         /*final TextView textView2 = findViewById(R.id.lblTemperature);
@@ -68,6 +103,8 @@ public class Todays_Weather extends AppCompatActivity {
 
 
     }
+
+
 }
 
 
